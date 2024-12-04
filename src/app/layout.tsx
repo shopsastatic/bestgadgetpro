c993bc210a049190128523e85e73648231433a13
Khai Dang <akhai7230@gmail.com>
2024-12-04 10:54:16 +0700

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {isEnabled && <PreviewNotice />}
-        {/* <Navigation /> */}
+        <Navigation />
         {children}
       </body>
     </html>
