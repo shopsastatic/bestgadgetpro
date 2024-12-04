1990526b7146c990c992844dfc2fe7c8ae454890
Khai Dang <akhai7230@gmail.com>
2024-12-04 10:53:46 +0700

    Update

--- next.config.js
+++ next.config.js
@@ -10,6 +10,38 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png|js|css|module.css)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ]
+      },
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, stale-while-revalidate=59'
+          }
+        ]
+      }
+    ]
+  },
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+
+  experimental: {
+    turbotrace: true,
+    optimizeCss: true,
+  }
 };
 
 module.exports = nextConfig;
